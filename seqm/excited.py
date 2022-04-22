e788d18a057042d125b58dd58969907a94e50881
ares201005 <ares201005@gmail.com>
2022-04-22 11:25:01 -0600

    minor update

--- seqm/excited.py
+++ seqm/excited.py
@@ -27,9 +27,57 @@ RAISE_ERROR_IF_SCF_BACKWARD_FAILS = False
 RAISE_ERROR_IF_SCF_FORWARD_FAILS = False
 #if true, raise error rather than ignore those non-convered molecules
 
-def kernel():
+def kernel(rh0,xi):
     # kernel of TDHF equations
+    # 
+    # xi Li(xi) == > L_ij
+    # Li = [F[rho0],x] + [V(xi),rho0]
+    #
+    # Vxi in A.O.
+    # one-body in M.O.
+    # 
+    # (A,   B) (x)  
+    # (-B, -A) (y)
+
+    #1) build Vxi in A.O.
+
+    #2) tansform Vxi from AO TO MO
+    # V^\dag A_AO V  --> A_MO
+    # 
+    #) add one-body term
 
     return None
+
+
+def Davidson(xi,matvec):
+
+    
+    """
+    for m in range(k,mmax,k):
+       if m <= k:
+           for j in range(0,k):
+               V[:,j] = t[:,j]/np.linalg.norm(t[:,j])
+           theta_old = 1 
+       elif m > k:
+           theta_old = theta[:eig]
+       V[:,:m],R = np.linalg.qr(V[:,:m])
+       T = np.dot(V[:,:m].T, matvec(V[:,:m]))
+
+       THETA,S = np.linalg.eig(T)
+       idx = THETA.argsort()
+       theta = THETA[idx]
+       s = S[:,idx]
+       for j in range(0,k):
+           w = np.dot((A - theta[j]*I),np.dot(V[:,:m],s[:,j])) 
+           q = w/(theta[j]-A[j,j])
+           V[:,(m+j)] = q
+       norm = np.linalg.norm(theta[:eig] - theta_old)
+       if norm < tol:
+           break
+   
+   #
+       return None
+    """
+
 
 
